38f92012355870ca107bd89e8bda32bdb91683db
Tess O <tessoceg@gmail.com>
2020-09-23 09:14:04 -0700

    feat(client/allproducts):allproducts component started

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -1,7 +1,36 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import {fetchProducts} from '../store/allProducts'
 
-export default class AllProducts extends React.Component {
+class AllProducts extends React.Component {
+  componentDidMount() {
+    this.props.fetchProducts()
+    console.log(this.props)
+  }
+
   render() {
-    return <div>Hello</div>
+    return (
+      <div>
+        {this.props.products.map(product => {
+          return <div>{product.name}</div>
+        })}
+      </div>
+    )
   }
 }
+
+const mapState = state => {
+  return {
+    products: state
+  }
+}
+
+const mapDispatch = dispatch => {
+  return {
+    fetchProducts: function() {
+      dispatch(fetchProducts())
+    }
+  }
+}
+
+export default connect(mapState, mapDispatch)(AllProducts)
---
+++ client/store/allProducts.js
@@ -0,0 +1,30 @@
+import axios from 'axios'
+
+const initialState = []
+
+//action types
+const FETCH_PRODUCTS = 'FETCH_PRODUCTS'
+const SET_PRODUCTS = 'SET_PRODUCTS'
+//action creators
+const setProducts = products => ({
+  type: SET_PRODUCTS,
+  products
+})
+//thunk creators
+export const fetchProducts = () => {
+  return async dispatch => {
+    const response = await axios.get('/api/products')
+    const products = response.data
+    const action = setProducts(products)
+    dispatch(action)
+  }
+}
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case SET_PRODUCTS:
+      return action.products
+    default:
+      return state
+  }
+}
