8eac9e786a53a21ade12202915a4cf304fd2bb7d
Tess O <tessoceg@gmail.com>
2020-09-24 11:57:51 -0700

    feat(client/api):added cart reducer on front end and order routes on backend

---
+++ client/store/cart.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+const initialState = []
+
+export default function cartReducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
