fbf0f4ac7d88d821a2c35f7e7c871bc7ec52ba9b
Ioana Medvesan <ioanamedvesan@yahoo.com>
2020-09-25 17:23:29 -0500

    fix(client/component/cart.js): add style to cart

--- client/components/cart.js
+++ client/components/cart.js
@@ -19,6 +19,11 @@ import {me} from '../store/user'
 // }
 
 export class Cart extends React.Component {
+  // const removeFromCart = (product) => {
+  //   const currentItem = this.state.currentCart.slice();
+  //   currentItem.filter(x=>x.id !== product.id)
+  // }
+
   async componentDidMount() {
     // console.log('props in componentDidMount', this.props)
     // let user = this.props.user
@@ -31,13 +36,54 @@ export class Cart extends React.Component {
     // console.log('user in render of cart component', this.props.user)
     // console.log('cart in render of cart component', this.props.cart)
     let currentCart = this.props.cart
+
     return (
+      // <div>
+      //   <div>Cart Items (from class component):</div>
+      //   {currentCart &&
+      //     currentCart.map(item => {
+      //       return <li key={item.id}>{item.name}</li>
+      //     })}
+      // </div>
       <div>
-        <div>Cart Items (from class component):</div>
-        {currentCart &&
-          currentCart.map(item => {
-            return <li key={item.id}>{item.name}</li>
-          })}
+        {currentCart.length === 0 ? (
+          <div className="cart cart-header">Cart is empty</div>
+        ) : (
+          <div className="cart cart-header">
+            You have {currentCart.length} products in the cart
+          </div>
+        )}
+        <div>
+          <div className="cart">
+            <ul className="cart-items">
+              {currentCart &&
+                currentCart.map(item => (
+                  <li key={item.id}>
+                    <div>
+                      <img src={item.image} alt={item.name} />
+                    </div>
+                    <div>{item.name}</div>
+                    <div className="right">
+                      ${item.price} x {item.count}
+                      <button
+                        className="button" /*onClick={() => this.props.removeFromCart(item)}*/
+                      >
+                        Remove
+                      </button>
+                    </div>
+                  </li>
+                ))}
+            </ul>
+          </div>
+          <div className="cart">
+            <div className="total">
+              <div>Total: 0</div>
+            </div>
+            <div>
+              <button className="button primary">Proceed</button>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
